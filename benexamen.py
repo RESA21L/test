9ef3b959f0b0f835b8c52de482e6aede2759e412
mikelimazulu <mikelimazulu@protonmail.com>
2021-09-29 11:00:48 -0400

    ben

---
+++ benexamen.py
@@ -0,0 +1,10 @@
+import random
+import time
+
+choices = ["Ben va corriger les examens aujourdhui", "Ben ne va pas corriger les examens aujourdhui"]
+
+answer = random.choice(choices)
+print("Est-ce que Ben corrigera les examens aujourdhui?")
+time.sleep(5)
+print(answer)
+print(choices[0])
